462bce6e5b00b22f9dc3842dbed819ccaf869ccf
Shikhar Batham <shikhar.batham@gmail.com>
2023-06-26 23:51:53 +0530

    second commit

--- src/main/java/com/crestdevs/BlogAppBE/service/UserService.java
+++ src/main/java/com/crestdevs/BlogAppBE/service/UserService.java
@@ -1,8 +1,7 @@
 package com.crestdevs.BlogAppBE.service;
 
 import com.crestdevs.BlogAppBE.payload.UserDto;
-import com.crestdevs.bogBE.payload.UserDto;
-import org.springframework.stereotype.Service;
+
 
 public interface UserService {
 
