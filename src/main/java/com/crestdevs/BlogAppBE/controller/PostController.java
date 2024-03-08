02c4dc8aee2bb4929a410161bc6e80d618da960b
shikhar-batham <83720643+shikhar-batham@users.noreply.github.com>
2024-03-08 06:49:10 +0530

    user profile image upload and download

--- src/main/java/com/crestdevs/BlogAppBE/controller/PostController.java
+++ src/main/java/com/crestdevs/BlogAppBE/controller/PostController.java
@@ -5,10 +5,15 @@ import com.crestdevs.BlogAppBE.payload.PostDto;
 import com.crestdevs.BlogAppBE.payload.PostResponse;
 import com.crestdevs.BlogAppBE.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -17,6 +22,9 @@ public class PostController {
 
     @Autowired
     private PostService postService;
+
+    @Value("${project.postImages}")
+    private String path;
 
     @PostMapping("/")
     public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto,
@@ -84,6 +92,25 @@ public class PostController {
         List<PostDto> postDtoList = this.postService.searchPost(keyword);
 
         return new ResponseEntity<>(postDtoList, HttpStatus.OK);
+    }
+
+    @PostMapping("/postImage/{postId}")
+    public ResponseEntity<PostDto> uploadPostImage(@PathVariable("postId") Integer postId, @RequestParam MultipartFile file) throws IOException {
+
+        PostDto postDto = this.postService.uploadPostImage(postId, path, file);
+
+        return new ResponseEntity<>(postDto, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/downloadPostImage/{postId}", produces = MediaType.IMAGE_JPEG_VALUE)
+    public void downloadPostImage(@PathVariable int postId, HttpServletResponse response) throws IOException{
+
+        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+        try {
+            this.postService.downloadPostImage(postId, path, response);
+        } catch (IOException ignored) {
+
+        }
     }
 
 }
